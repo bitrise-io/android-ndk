15c2e0ed0d67efd471810d41f6377e338e4a0d84
Viktor Benei <viktor.benei@gmail.com>
2016-06-17 12:21:28 +0200

    tree /opt/android-ndk

--- system_report.sh
+++ system_report.sh
@@ -17,3 +17,6 @@ fi
 echo "* BITRISE_DOCKER_REV_NUMBER_ANDROID_NDK: $BITRISE_DOCKER_REV_NUMBER_ANDROID_NDK"
 echo "========================================"
 echo
+echo "$ tree -L 2 /opt/android-ndk"
+tree -L 2 /opt/android-ndk
+echo
